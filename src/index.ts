2d84322ef6eab933ec87681567a93ec73fe27e70
Romain Laurence <mw3pumat8@gmail.com>
2023-11-29 02:16:02 +0100

    encapsulation du projet dans docker compose

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import categoriesRouter from "./categories";
 import { query } from "./db";
 
-
 const todos = [
   {
     todoId: 1,
@@ -48,53 +47,65 @@ app.use("/categories", categoriesRouter);
 
 // on définit une route GET /todos, et la fonction qui s'exécute lorsque le serveur reçoit une requête qui matche
 app.get("/todos", async (request, result) => {
-  const todosDB = await query('SELECT * FROM todos');
-  console.table(todosDB)
+  const todosDB = await query("SELECT * FROM todos");
+  console.table(todosDB);
   console.log(`route "/todos" called`);
   return result.status(200).json(todosDB);
 });
 
 // une autre route pour récupérer 1 TODO
-app.get("/todos/:id", async (request: express.Request, result: express.Response) => {
-  console.log(`route "/todos/:id" called`);
-  console.log(`params of the request : ${JSON.stringify(request.params)}`);
-  const todoDB = await query(`SELECT * FROM todos WHERE id = ${request.params.id}`);
-  console.log(todoDB)
-  return result
-    .status(200)
-    .json(
-      todoDB || null,
+app.get(
+  "/todos/:id",
+  async (request: express.Request, result: express.Response) => {
+    console.log(`route "/todos/:id" called`);
+    console.log(`params of the request : ${JSON.stringify(request.params)}`);
+    const todoDB = await query(
+      `SELECT * FROM todos WHERE id = ${request.params.id}`
     );
-});
+    console.log(todoDB);
+    return result.status(200).json(todoDB || null);
+  }
+);
 
 app.put("/todos/:id", async (req: express.Request, res: express.Response) => {
   const id = Number(req.params.id);
   console.log(`put "/todos/:id" called`, req.body, id);
   const text = req.body.text.replaceAll("'", "\\'");
-  const todo = await query(`UPDATE todos SET text = "${text}", done = ${Boolean(req.body.done)} WHERE id = ${id}`);
-  return res.status(200).json({status: 'OK'})
+  const todo = await query(
+    `UPDATE todos SET text = '${text}', done = ${Boolean(
+      req.body.done
+    )} WHERE id = ${id}`
+  );
+  return res.status(200).json({ status: "OK" });
 });
 
 app.post("/todos", async (req: express.Request, res: express.Response) => {
   const newTodo = req.body;
-  const lastTodoId = await query('SELECT MAX(id) as maxId FROM todos');
+  const lastTodoId = await query("SELECT MAX(id) as maxId FROM todos");
   newTodo.id = lastTodoId[0].maxId + 1;
 
-  await query(`INSERT INTO todos (text, done, id_categorie) VALUES ('${String(newTodo.text)}',${Boolean(newTodo.done)}, ${Number(newTodo.category)})`)
-  console.log(newTodo)
+  await query(
+    `INSERT INTO todos (text, done, id_categorie) VALUES ('${String(
+      newTodo.text
+    )}',${Boolean(newTodo.done)}, ${Number(newTodo.category)})`
+  );
+  console.log(newTodo);
   return res.status(200).json(newTodo);
 });
 
-app.delete("/todos/:id", async (req: express.Request, res: express.Response) => {
-  const id = Number(req.params.id);
-  console.log('Route Delete called on id :', id)
-  const response = await query(`DELETE FROM todos WHERE id = ${id}`)
-  //const index = todos.findIndex((todo) => todo.todoId === id);
-  if (!response) {
-    return res.status(404).json(null);
-  } else {
-    return res.status(200).json({ ok: true });
+app.delete(
+  "/todos/:id",
+  async (req: express.Request, res: express.Response) => {
+    const id = Number(req.params.id);
+    console.log("Route Delete called on id :", id);
+    const response = await query(`DELETE FROM todos WHERE id = ${id}`);
+    //const index = todos.findIndex((todo) => todo.todoId === id);
+    if (!response) {
+      return res.status(404).json(null);
+    } else {
+      return res.status(200).json({ ok: true });
+    }
   }
-});
+);
 
 app.listen(8080, () => console.log("server started, listening on port 8080"));
